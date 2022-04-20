ba56d1fa2eeff9a491332756775cdd3c39a89959
Luis Locon <luis.locon@gmail.com>
2022-04-19 21:51:07 -0600

    declaracion de variables

--- src/main.go
+++ src/main.go
@@ -3,6 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
-}
+	// Declaracion de constantes
+	const pi float64 = 3.14159265358979323846
+	const pi2 = 3.1416
+	fmt.Println("pi: ", pi)
+	fmt.Println("pi2: ", pi2)
 
+	// Declaracion de variables enteras
+	base := 12
+	var altura int = 24
+	var area int
+	fmt.Println(base, altura, area)
+
+	//Zero values
+	//Go asigna vaalores a variables vacías
+	var a int     // 0
+	var b float64 // 0
+	var c string  // " " blank space
+	var d bool    // false
+	fmt.Println(a, b, c, d)
+
+	// Area de un cuadrado
+	const baseCuadrado = 10
+	areaCuadrado := baseCuadrado * baseCuadrado
+	fmt.Println("Area cuadrado: ", areaCuadrado)
+}
