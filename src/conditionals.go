c25c271a76866c50767e75cb0909e41905c7537f
Luis Locon <luis.locon@gmail.com>
2022-04-20 20:58:54 -0600

    switch

--- src/conditionals.go
+++ src/conditionals.go
@@ -13,4 +13,13 @@ func main() {
 	} else {
 		fmt.Println("Value1 is equal to Value2")
 	}
+
+	// Switch
+	modulo := 5 % 2
+	switch modulo {
+	case 0:
+		fmt.Println("Modulo is 0 and is par")
+	default:
+		fmt.Printf("Modulo is %d and is impar\n", modulo)
+	}
 }
